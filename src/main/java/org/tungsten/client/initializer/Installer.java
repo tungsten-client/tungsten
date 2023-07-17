966329ed3296899cbe143f7571e316450c743137
HyperCodec <72839119+HyperCodec@users.noreply.github.com>
2023-07-17 11:02:25 -0400

    put in some debug

--- src/main/java/org/tungsten/client/initializer/Installer.java
+++ src/main/java/org/tungsten/client/initializer/Installer.java
@@ -1,5 +1,6 @@
 package org.tungsten.client.initializer;
 
+import org.tungsten.client.Tungsten;
 import org.tungsten.client.util.WebUtils;
 
 import java.io.File;
@@ -15,13 +16,17 @@ public class Installer {
     final static String JDKPath = String.format("%s/%s", TMP, JDKFileName);
 
     public static void run() throws IOException, InterruptedException {
+        Tungsten.LOGGER.info(String.format("Checking for JDK %s...", JavaVersion));
         // TODO check if jdk exists and return if it does
+        Tungsten.LOGGER.info("JDK not found, starting installation process...");
 
         // if jdk not found, install
+        Tungsten.LOGGER.info(String.format("Downloading JDK '%s' to '%s'...", JavaVersion, JDKPath));
         File f = new File(JDKPath);
         WebUtils.downloadURLToPath(JDKURL, f);
 
         // run installer
+        Tungsten.LOGGER.info("Running JDK installer executable...");
         ProcessBuilder pb = new ProcessBuilder(JDKPath);
         Process p = pb.start();
         p.waitFor();
