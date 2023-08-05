95f48623feb3c3b78972dfccf19dabc9ab6ec419
TangyKiwi <orange.panda.65@gmail.com>
2023-08-05 14:01:43 -0700

    Add module version of ClickGUI w/ rshift keybind

---
+++ src/main/java/org/tungsten/client/feature/module/modules/client/mClickGUI.java
@@ -0,0 +1,30 @@
+package org.tungsten.client.feature.module.modules.client;
+
+import org.lwjgl.glfw.GLFW;
+import org.tungsten.client.feature.module.GenericModule;
+import org.tungsten.client.gui.clickgui.ClickGUI;
+
+public class mClickGUI extends GenericModule {
+    public mClickGUI() {
+        super("ClickGUI", "Enables the ClickGUI", "CLIENT");
+        this.updateKeybind(GLFW.GLFW_KEY_RIGHT_SHIFT);
+    }
+
+    @Override
+    protected void disable() {
+
+    }
+
+    @Override
+    protected void enable() {
+        ClickGUI gui = ClickGUI.create();
+        this.client.setScreen(gui);
+        gui.reload();
+        setEnabled(false);
+    }
+
+    @Override
+    protected void tickClient() {
+
+    }
+}
