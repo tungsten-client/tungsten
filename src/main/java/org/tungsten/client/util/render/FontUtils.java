48cb15b870a738bcd1454733a6681cbf3d284bea
bruh1273 <69420hahafuni@gmail.com>
2023-10-31 17:47:16 -0600

    Fix Fonts

--- src/main/java/org/tungsten/client/util/render/FontUtils.java
+++ src/main/java/org/tungsten/client/util/render/FontUtils.java
@@ -1,21 +1,26 @@
 package org.tungsten.client.util.render;
 
+import org.tungsten.client.Tungsten;
+
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class FontUtils {
 
     public static Font primaryFont;
     public static Font secondaryFont;
     public static Font backupFont;
-
     static {
         try {
-            primaryFont = Font.createFont(Font.PLAIN, new File("C:\\Users\\burke\\IdeaProjects\\tongueson\\src\\main\\resources\\assets\\tungsten\\font\\TitilliumWeb-SemiBold.ttf").getAbsoluteFile());
-            secondaryFont = Font.createFont(Font.PLAIN, new File("C:\\Users\\burke\\IdeaProjects\\tongueson\\src\\main\\resources\\assets\\tungsten\\font\\Tajawal-Regular.ttf").getAbsoluteFile());
-            backupFont = Font.createFont(Font.PLAIN, new File("C:\\Users\\burke\\IdeaProjects\\tongueson\\src\\main\\resources\\assets\\tungsten\\font\\OpenSans-Regular.ttf").getAbsoluteFile());
-        } catch (FontFormatException | IOException e) { throw new RuntimeException(e); }
+            primaryFont = Font.createFont(Font.TRUETYPE_FONT, new File(Tungsten.class.getResource("/assets/tungsten/fonts/TitilliumWeb-SemiBold.ttf").toURI()));
+            secondaryFont = Font.createFont(Font.PLAIN, new File(Tungsten.class.getResource("/assets/tungsten/fonts/Tajawal-Regular.ttf").toURI()));
+            backupFont = Font.createFont(Font.PLAIN, new File(Tungsten.class.getResource("/assets/tungsten/fonts/OpenSans-Regular.ttf").toURI()));
+        } catch (FontFormatException | IOException | URISyntaxException e) { throw new RuntimeException(e); }
     }
 
 }
